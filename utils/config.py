8e0b5c2de6e63158bb7f5fa8a799c26cedd2bfc4
Ziyunp <ziyun_poh@hotmail.com>
2020-06-09 12:29:07 +0100

    Changed data path

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,9 @@
-data_path = "./data"
+data_path = "../data_to_run_code_on"
+
+# TODO:
+# generalise paths
 
 PATHS = {
-  "all_data": "{}/all_training_data_pings_labels_incl_5km.csv".format(data_path)
+  "all_data": "{}/all_training_data_pings_labels_incl_5km.csv".format(data_path),
+  "labelled_data": "{}/20191002-20200130_isotrak_labelled.csv".format(data_path)
 } 
