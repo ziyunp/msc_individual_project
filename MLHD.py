fe6b9f6fd8a0201de367ae14ee95a73538d190bf
Ziyunp <ziyun_poh@hotmail.com>
2020-06-12 23:58:17 +0100

    Added collective_perp_distance

--- MLHD.py
+++ MLHD.py
@@ -90,6 +90,17 @@ def parallel_distance_btw_two_lines(lm, ln):
     return lf.get_parallel_distance(lm, ln)
   return lf.get_parallel_distance(ln, lm)
 
+def collective_perpendicular_distance(lm, N_lines):
+  """
+    Calculates the parallel distance between a line and the set of neighboring lines
+    :param lm: line, N_lines: the set of neighboring lines of lm
+    :return: parallel distance between a line and the set of neighboring lines
+  """
+  total_perp_distance = 0
+  for ln in N_lines:
+    total_perp_distance += perpendicular_distance_btw_two_lines(lm, ln)
+  return total_perp_distance
+
 def collective_parallel_distance(lm, N_lines):
   """
     Calculates the parallel distance between a line and the set of neighboring lines
