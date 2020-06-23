41bbee302079cb073c76eddad0f65b15520035f7
Ziyunp <ziyun_poh@hotmail.com>
2020-06-23 22:09:17 +0100

    (draft) Append road names where nan

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
 import logging
 import os
 import utils.column_names as cn
 import utils.config as config
-
+import utils.tree as tree
 """
   Filter legs with two consecutive pings of more than 10 min away
 """
@@ -11,6 +12,7 @@ log = logging.getLogger(__name__)
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO")) #added
 
 MISSING_PING_THRESHOLD = 10 # defines a missing ping if two consecutive pings are >= 10 min apart
+ROAD_THRESHOLD = 0.25 # km
 
 def has_missing_pings(data):
   total = len(data)
@@ -22,35 +24,65 @@ def has_missing_pings(data):
       return True
   return False
 
+def assign_road(data, roads_tree, roads_data):
+  points = np.array(list(zip(data[cn.EVENT_LAT], data[cn.EVENT_LONG])))
+  distance, indices = roads_tree.query(points, k=1)
+  road_names = []
+  for d, i in zip(distance, indices):
+    if d <= ROAD_THRESHOLD:
+      road_names.append(roads_data.iloc[i][cn.ROAD])
+    else:
+      road_names.append("unknown")
+  return road_names
+
+def fill_in_missing_road(gps_data, roads_data):
+  roads_coords = np.dstack([roads_data.latitude.ravel(), roads_data.longitude.ravel()])[0]
+  roads_tree = tree.construct_balltree(roads_coords)
+
+  labelled_pings = gps_data[gps_data[cn.ROAD_NAME].notna()]
+  unlabelled_pings = gps_data[gps_data[cn.ROAD_NAME].isna()]
+  # match unlabelled with roads_data
+  road_names = assign_road(unlabelled_pings, roads_tree, roads_data)
+  assert len(road_names) == len(unlabelled_pings)
+  # print(road_names)
+  # compare to those labelled and clean 
+  # gps_data.loc[gps_data[cn.ROAD_NAME].isna(), cn.ROAD_NAME] = road_names
+  # print(gps_data)
+
 def main():
-  data_file = config.FILENAMES["all_data"] # file stored locally
+  data_file = config.FILENAMES["all_data_with_road_names"] # file stored locally
   log.info("Reading in data with leg_ids from  from {}".format(data_file))
 
   data = pd.read_csv(data_file, parse_dates=[cn.EVENT_DTTM])
-  legs = data[cn.LEG_ID].unique()
+  # legs = data[cn.LEG_ID].unique()
 
-  log.info("Initial number of pings: {}".format(len(data)))
-  log.info("{} distinct leg_ids from this dataset".format(len(legs)))
+  # log.info("Initial number of pings: {}".format(len(data)))
+  # log.info("{} distinct leg_ids from this dataset".format(len(legs)))
 
-  log.info("Scanning each leg for missing pings...")
+  # log.info("Scanning each leg for missing pings...")
   
-  legs_with_missing_pings = []
-  for leg_id in legs:
-    data_sub = data[data[cn.LEG_ID] == leg_id]
-    if has_missing_pings(data_sub):
-      legs_with_missing_pings.append(leg_id)
+  # legs_with_missing_pings = []
+  # for leg_id in legs:
+  #   data_sub = data[data[cn.LEG_ID] == leg_id]
+  #   if has_missing_pings(data_sub):
+  #     legs_with_missing_pings.append(leg_id)
   
-  log.info("{} legs have two consecutive pings that are >= {} min apart".format(len(legs_with_missing_pings), MISSING_PING_THRESHOLD))
+  # log.info("{} legs have two consecutive pings that are >= {} min apart".format(len(legs_with_missing_pings), MISSING_PING_THRESHOLD))
 
-  log.info("Removing legs with missing pings...")
-  for leg_id in legs_with_missing_pings:
-    data = data[data[cn.LEG_ID] != leg_id]
+  # log.info("Removing legs with missing pings...")
+  # for leg_id in legs_with_missing_pings:
+  #   data = data[data[cn.LEG_ID] != leg_id]
 
-  log.info("Final number of pings: {}".format(len(data)))
-  log.info("{} leg_ids at end of classification".format(data[cn.LEG_ID].nunique()))
+  # log.info("Final number of pings: {}".format(len(data)))
+  # log.info("{} leg_ids at end of classification".format(data[cn.LEG_ID].nunique()))
 
-  # Generate the export path for the isotrak file with road labels
-  export_path = config.FILENAMES["all_data_cleaned"] 
+  if cn.ROAD_NAME in data.columns:
+    log.info("Filling in missing road names...")
+    roads_data_file = config.FILENAMES["roads_data"]
+    roads_data = pd.read_csv(roads_data_file, dtype={cn.LINK_NAME: str, cn.C_WAY: str, cn.SECTION: str})
+    data = fill_in_missing_road(data, roads_data)
+
+  export_path = config.FILENAMES["all_data_with_road_names_cleaned"] 
   log.info("Writing out classified data to {}".format(export_path))
   data.to_csv(export_path, index=False)
   log.info("Preprocessing done")
--- utils/config.py
+++ utils/config.py
@@ -10,4 +10,5 @@ FILENAMES = {
   "labelled_data": "{}/20191002-20200130_isotrak_labelled.csv".format(data_path),
   "labelled_subset": "{}/20191002-20200130_subset_isotrak_labelled.csv".format(data_path),
   "test_excl_5km": "{}/20191002-20200130_isotrak_legs_excl_5km_test.csv".format(data_path),
+  "roads_data": "{}/roads_data.csv".format(data_path)
 } 
