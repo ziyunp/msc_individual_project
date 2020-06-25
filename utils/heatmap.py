086a89b3dd12052fc132eea1fa2e6f245e9c5726
Ziyunp <ziyun_poh@hotmail.com>
2020-06-25 18:23:17 +0100

    Order legs by clusters in heatmap

--- utils/heatmap.py
+++ utils/heatmap.py
@@ -4,11 +4,17 @@ import matplotlib.pyplot as plt
 
 def main():
   for i in range(1, 10):
-    # Group legs from the same cluster together,
-
+    labels_file = "labels_" + str(i) + ".csv"
     distance_file = "distance_matrix_" + str(i) + ".csv"
+    labels = np.loadtxt(labels_file, delimiter=",")
     distance_matrix = np.loadtxt(distance_file, delimiter=",")
-    heat_map = plt.imshow(distance_matrix, cmap="hot", interpolation="nearest")
+    # Rearrange matrix to order legs by cluster
+    ordered_indices = np.argsort(labels)
+    length = labels.shape[0]
+    order_rows = np.tile(ordered_indices, (length, 1))
+    ordered_matrix = np.array(list(map(lambda x, y: y[x], order_rows, distance_matrix)))
+    ordered_matrix = ordered_matrix[ordered_indices]
+    heat_map = plt.imshow(ordered_matrix, cmap="hot", interpolation="nearest")
     plt.show()
 
 if __name__ == "__main__":
