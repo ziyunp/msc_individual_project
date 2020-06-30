59cc8bbcd552934b863bd89c7b1d854bf06fe385
Ziyunp <ziyun_poh@hotmail.com>
2020-06-30 16:33:02 +0100

    Added locate_elbow function

--- plot_kdist.py
+++ plot_kdist.py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import logging
 import os
+from kneed import KneeLocator
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO")) #added
 
-def plot_kdist(distance, figName, k = 3):
+def save_kdist_plot(distance, figName, k = 3):
     log.info("Plotting {}...".format(figName))
     nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto', metric='precomputed').fit(distance)
     distances, indices = nbrs.kneighbors(distance)
@@ -20,3 +21,14 @@ def plot_kdist(distance, figName, k = 3):
     fig.savefig(figName, dpi=fig.dpi)
     log.info("Saved k-dist plot to {}".format(figName))  
 
+def locate_elbow(distance, figName, k=3):
+    log.info("Locating elbow...")
+    nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto', metric='precomputed').fit(distance)
+    distances, indices = nbrs.kneighbors(distance)
+    kth_dist = distances[:,-1]
+    kth_dist.sort()
+    x = indices[:,0]
+    y = kth_dist
+    kneedle = KneeLocator(x, y, curve='convex', direction='increasing', interp_method='polynomial')
+    kneedle.plot_knee()
+    return kneedle.elbow_y
