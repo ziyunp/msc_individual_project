3107eacb9c019f7e3a21dc5b522291382a5debb8
Ziyunp <ziyun_poh@hotmail.com>
2020-07-01 18:22:02 +0100

    Find multiple eps values

--- plot_kdist.py
+++ plot_kdist.py
@@ -8,27 +8,37 @@ from kneed import KneeLocator
 log = logging.getLogger(__name__)
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO")) #added
 
-def save_kdist_plot(distance, figName, k = 3):
-    log.info("Plotting {}...".format(figName))
-    nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto', metric='precomputed').fit(distance)
-    distances, indices = nbrs.kneighbors(distance)
-    kth_dist = distances[:,-1]
-    kth_dist.sort()
-    x = indices[:,0]
-    y = kth_dist
-    fig = plt.figure()
-    plt.plot(x, y)
-    fig.savefig(figName, dpi=fig.dpi)
-    log.info("Saved k-dist plot to {}".format(figName))  
-
 def locate_elbow(distance, figName, k=3):
     nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto', metric='precomputed').fit(distance)
     distances, indices = nbrs.kneighbors(distance)
     kth_dist = distances[:,-1]
     kth_dist.sort()
-    x = indices[:,0]
-    y = kth_dist
-    kneedle = KneeLocator(x, y, curve='convex', direction='increasing', interp_method='polynomial')
-    kneedle.plot_knee()
-    # plt.show()
-    return kneedle.elbow_y
+    x_data = indices[:,0]
+    y_data = kth_dist
+    # Find elbows recursively until x contains less than 4 points (min points to form an elbow)
+    x_start = 0
+    elbows = []
+    elbows_x = []
+    while x_start != None and x_start < len(x_data) - 4:
+        x = x_data[x_start:]
+        y = y_data[x_start:]
+        kneedle = KneeLocator(x, y, curve='convex', direction='increasing', interp_method='polynomial')
+        # TODO: should we take the last value in y as an elbow value? - the furthest distance from kth neighbours => will include all points
+        elbows.append(kneedle.elbow_y)
+        x_start = kneedle.elbow
+        elbows_x.append(kneedle.elbow)
+        # kneedle.plot_knee()
+        # plt.show()
+
+    # Plot all knees
+    kl = KneeLocator(x_data, y_data, curve='convex', direction='increasing', interp_method='polynomial')
+    plt.style.use('ggplot')
+    fig = plt.figure(figsize=(6, 6))
+    plt.title("Default")
+    plt.plot(kl.x, kl.y, "b", label="data")
+    colors = ['r', 'g', 'k', 'm', 'c', 'orange']
+    for k, c, s in zip(elbows_x, colors, elbows):
+        plt.vlines(k, plt.ylim()[0], plt.ylim()[1], linestyles='--', colors=c, label=f'eps = {s}')
+    plt.legend(loc="best")
+    fig.savefig(figName, dpi=fig.dpi)
+    return elbows
