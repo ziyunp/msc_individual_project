d8fbf13e5a863240755ea5c76f78e8e1e71d1ef6
Ziyunp <ziyun_poh@hotmail.com>
2020-07-06 13:42:46 +0100

    Refactor to use the same plot

--- plot_kdist.py
+++ plot_kdist.py
@@ -22,27 +22,21 @@ def locate_elbow(distance, figName, multiple=False, k=4):
         kneedle = KneeLocator(x_data, y_data, curve='convex', direction='increasing', interp_method='polynomial')
         if kneedle.elbow_y != None:
             elbows.append(kneedle.elbow_y)
-        kneedle.plot_knee()
-        plt.show()
-        return elbows
-    
-    # Find multiple elbows 
-    x_start = 0
-    elbows_x = []
-    # Min num of points to form an elbow = 3
-    while x_start != None and x_start < len(x_data) - 3:
-        x = x_data[x_start:]
-        y = y_data[x_start:]
-        kneedle = KneeLocator(x, y, curve='convex', direction='increasing', interp_method='polynomial')
-        if kneedle.elbow_y != None and kneedle.elbow_y != y_data[-1]:
-            if elbows and kneedle.elbow_y / elbows[-1] < 2:
-                # If this elbow is less than double the previous elbow, take the later elbow
-                elbows[-1] = kneedle.elbow_y
-                elbows_x[-1] = kneedle.elbow
-            else:
-                elbows.append(kneedle.elbow_y)
-                elbows_x.append(kneedle.elbow)
-        x_start = kneedle.elbow
+    else:     
+        # Find multiple elbows 
+        x_start = 0
+        # Min num of points to form an elbow = 3
+        while x_start != None and x_start < len(x_data) - 3:
+            x = x_data[x_start:]
+            y = y_data[x_start:]
+            kneedle = KneeLocator(x, y, curve='convex', direction='increasing', interp_method='polynomial')
+            if kneedle.elbow_y != None and kneedle.elbow_y != y_data[-1]:
+                if elbows and kneedle.elbow_y / elbows[-1] < 2:
+                    # If this elbow is less than double the previous elbow, take the later elbow
+                    elbows[-1] = kneedle.elbow_y
+                else:
+                    elbows.append(kneedle.elbow_y)
+            x_start = kneedle.elbow
 
     # Plot all knees
     kl = KneeLocator(x_data, y_data, curve='convex', direction='increasing', interp_method='polynomial')
@@ -51,8 +45,8 @@ def locate_elbow(distance, figName, multiple=False, k=4):
     plt.title("Default")
     plt.plot(kl.x, kl.y, "b", label="data")
     colors = ['r', 'g', 'k', 'm', 'c', 'orange']
-    for k, c, s in zip(elbows_x, colors, elbows):
-        plt.vlines(k, plt.ylim()[0], plt.ylim()[1], linestyles='--', colors=c, label=f'eps = {s}')
+    for k, c, s in zip(elbows, colors, elbows):
+        plt.hlines(k, plt.xlim()[0], plt.xlim()[1], linestyles='--', colors=c, label=f'eps = {s}')
     plt.legend(loc="best")
     fig.savefig(figName, dpi=fig.dpi)
     return elbows
