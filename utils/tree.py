39c276db5ca8ae5949e65176e17abe3d735b240f
Ziyunp <ziyun_poh@hotmail.com>
2020-07-28 14:40:26 +0100

    Added param to sort output

--- utils/tree.py
+++ utils/tree.py
@@ -10,10 +10,10 @@ def construct_balltree(points):
 def construct_kdtree(points):
   return KDTree(points)
 
-def query_balltree_radius(tree, points, radius, ret_distance=False):
+def query_balltree_radius(tree, points, radius, ret_distance=False, sort=False):
   points = list(map(hp.convert_coords_to_radians, points))
   radius = radius / config.CONSTANTS["earth_radius"] # convert to radians
-  return tree.query_radius(points, r=radius, return_distance=ret_distance)
+  return tree.query_radius(points, r=radius, return_distance=ret_distance, sort_results=sort)
 
 def query_balltree_knn(tree, points, _k, ret_distance=False):
   points = list(map(hp.convert_coords_to_radians, points))
