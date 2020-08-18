5ba8df108f6285d133990cb68d9e4b63627ad655
Ziyunp <ziyun_poh@hotmail.com>
2020-08-18 13:39:15 +0100

    Added other filepaths

--- utils/config.py
+++ utils/config.py
@@ -1,26 +1,25 @@
-# TODO:
-# generalise FILENAMES
+# path to the directory where the data is
 data_path = "../data_to_run_code_on" 
 heatmap_data = "../data/Heatmap"
-distances = "/distances/distance_matrix_"
 
 FILENAMES = {
   "all_data": "{}/all_training_data_pings_labels_incl_5km.csv".format(data_path),
+  "all_data_excl_5km": "{}/all_training_data_pings_labels_excl_5km.csv".format(data_path),
   "all_data_with_road_names": "{}/all_training_data_pings_labels_incl_5km_with_road_names.csv".format(data_path),
   "all_data_cleaned": "{}/all_training_data_pings_labels_incl_5km_cleaned.csv".format(data_path),
+  "all_data_excl_5km_cleaned": "{}/all_training_data_pings_labels_excl_5km_cleaned.csv".format(data_path),
   "all_data_clustered": "{}/all_training_data_pings_labels_incl_5km_clustered.csv".format(data_path),
   "all_data_with_road_names_cleaned": "{}/all_training_data_pings_labels_incl_5km_with_road_names_cleaned.csv".format(data_path),
   "labelled_data": "{}/20191002-20200130_isotrak_labelled.csv".format(data_path),
   "labelled_subset": "{}/20191002-20200130_subset_isotrak_labelled.csv".format(data_path),
   "test_excl_5km": "{}/20191002-20200130_isotrak_legs_excl_5km_test.csv".format(data_path),
+  "dist_mat_RM": "{}/dist_mats_from_hd_labels_train/dist_mat_".format(data_path),
+  "roads_data": "{}/roads_data.csv".format(data_path)
 } 
 
 DATA_FOR_HEATMAP = {
   "labels": "{}/labels/labels_".format(heatmap_data),
-  "basicHD_distances": "{}/basicHD_heatmap{}".format(heatmap_data, distances),
-  "MLHD_distances": "{}/MLHD_heatmap{}".format(heatmap_data, distances),
-  "MLHD_no_length_distances": "{}/MLHD_no_length_restriction{}".format(heatmap_data, distances),
-  "balltree_distances": "{}/balltree_heatmap{}".format(heatmap_data, distances),
+  "distances": "{}/distances/distance_matrix_".format(heatmap_data)
 }
 
 CONSTANTS = {
